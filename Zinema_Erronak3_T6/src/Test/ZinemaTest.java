0b290be76743263afd06a8eedb17fec975d9234a
Hodei <hodei.martinezag@elorrieta-errekamari.com>
2023-01-30 10:39:48 +0100

    zinema test zuzenketa

---
+++ Zinema_Erronak3_T6/src/Test/ZinemaTest.java
@@ -0,0 +1,13 @@
+package Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class ZinemaTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
