e0f953a10345428189f3c5e7580ab9e603b1f992
Aritza Herrero <aritz.herreroni@elorrieta-errekamari.com>
2023-02-07 12:36:26 +0100

    konexioa

---
+++ Zinema_Erronak3_T6/src/Controlador/datuBase.java
@@ -0,0 +1,31 @@
+package Controlador;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class datuBase {
+	
+	public static Connection konektatuDB() {
+        Connection connection = null;
+        try {  
+          // Datu-basearekin konektatzea
+          connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/zinema", "root", "");
+        } catch (SQLException e) {
+          System.out.println("Ezin izan da datu basera konektatu: " + e.getMessage());
+        }
+        return connection;
+      }
+	
+	 public static void deskonektatuDB(Connection connection) {
+	        try {
+	          if (connection != null) {
+	            connection.close();
+	            connection = null;
+	          }
+	        } catch (SQLException e) {
+	          e.printStackTrace();
+	        }
+	      }
+
+}
